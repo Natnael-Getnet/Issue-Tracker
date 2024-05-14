b060a8e7b3d2930080e93dc2705904d448a15d05
Natnael Getnet <NatnaelGetnetHaregewoyn@gmail.com>
2024-05-14 14:35:35 +0300

    added a markdown editor for the description

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
         <Theme>
           <NavBar />
           <main className="p-5">{children}</main>
-          <ThemePanel />
+          {/* <ThemePanel /> */}
         </Theme>
       </body>
     </html>
