8fa5fdf40d065b66d6ebb9cb87810a6d02b90ab5
Natnael Getnet <NatnaelGetnetHaregewoyn@gmail.com>
2024-05-16 18:13:27 +0300

    Added a dialog box for deleting an issue.

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,13 +1,39 @@
+"use client";
+
 import Link from "@/app/components/Link";
-import { Pencil2Icon, TrashIcon } from "@radix-ui/react-icons";
-import { Button } from "@radix-ui/themes";
+import { TrashIcon } from "@radix-ui/react-icons";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 
 const DeleteIssueButton = ({ id }: { id: number }) => {
   return (
-    <Button color="red">
-      <TrashIcon />
-      <Link href={`/issues/${id}/delete`}>Delete issue</Link>
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">
+          <TrashIcon />
+          Delete issue
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Delition</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this issue? This action ca not be
+          reverted!
+        </AlertDialog.Description>
+        <Flex mt="4" gap="3">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+
+          <AlertDialog.Action>
+            <Button color="red" onClick={() => console.log("delete")}>
+              Delete
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 
