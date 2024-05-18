e53e378081c5f8e85fdfae2847ae5960fe09c8e0
Natnael Getnet <NatnaelGetnetHaregewoyn@gmail.com>
2024-05-18 09:48:13 +0300

    Populate the assignee select component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,13 +1,32 @@
+"use client";
+
+import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import axios from "axios";
+import { use, useEffect, useState } from "react";
 
 const AssigneeSelect = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    async function getUsers() {
+      const { data } = await axios.get<User[]>("/api/users");
+      setUsers(data);
+    }
+    getUsers();
+  }, []);
+
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assign ..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="1">Natnael Getnet</Select.Item>
+          {users.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
