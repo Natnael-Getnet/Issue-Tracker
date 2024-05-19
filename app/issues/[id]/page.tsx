fbe2aa023b9e23f88c8ee70eb78e707a1d210329
Natnael Getnet <NatnaelGetnetHaregewoyn@gmail.com>
2024-05-19 11:35:17 +0300

    Optimized performace using react cache

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -5,14 +5,19 @@ import AssigneeSelect from "./AssigneeSelect";
 import DeleteIssueButton from "./DeleteIssueButton";
 import EditIssueButton from "./EditIssueButton";
 import { IssueDetails } from "./IssueDetails";
+import { cache } from "react";
+
+const fetchIssues = cache((issueId: number) =>
+  prisma.issue.findUnique({
+    where: { id: issueId },
+  })
+);
 
 interface Props {
   params: { id: string };
 }
 const page = async ({ params: { id } }: Props) => {
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(id) },
-  });
+  const issue = await fetchIssues(parseInt(id));
 
   if (!issue) return notFound();
 
@@ -33,9 +38,7 @@ const page = async ({ params: { id } }: Props) => {
 };
 
 export async function generateMetadata({ params }: Props) {
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
-  });
+  const issue = await fetchIssues(parseInt(params.id));
 
   if (!issue) return notFound();
 
