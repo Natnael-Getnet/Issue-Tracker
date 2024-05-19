e96a248b318e81bc67862143b4806e42c08f82f0
Natnael Getnet <NatnaelGetnetHaregewoyn@gmail.com>
2024-05-19 11:01:08 +0300

    Build the issue summary components

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,33 @@
+import prisma from "@/prisma/client";
 import LatestIssues from "./LatestIssues";
+import IssueSummary from "./IssueSummary";
 
-export default function Home({
+export default async function Home({
   searchParams,
 }: {
   searchParams: { page: string };
 }) {
+  const open = await prisma.issue.count({
+    where: {
+      status: "OPEN",
+    },
+  });
+
+  const inProgress = await prisma.issue.count({
+    where: {
+      status: "IN_PROGRESS",
+    },
+  });
+
+  const closed = await prisma.issue.count({
+    where: {
+      status: "CLOSED",
+    },
+  });
+
   return (
     <>
+      <IssueSummary open={open} inProgress={inProgress} closed={closed} />
       <LatestIssues />
     </>
   );
